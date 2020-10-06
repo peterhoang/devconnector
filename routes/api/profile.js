044af2c88c5fa0ac922d3f03a9904a7c16cfd958
Peter Hoang <peter.w.hoang@gmail.com>
2020-10-06 15:22:21 -0400

    Add put profile experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -165,4 +165,66 @@ router.delete('/', auth, async (req, res) => {
   }
 });
 
+// @route   Put api/profile/experience
+// @desc    Add and update profile experience
+// @access  Private
+router.put(
+  '/experience',
+  [
+    auth,
+    [
+      body('title', 'Title is required').not().isEmpty(),
+      body('company', 'Company is required').not().isEmpty(),
+      body('from', 'From date is required').not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description,
+    } = req.body;
+
+    const newExp = {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description,
+    };
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+
+      var foundExperience = profile.experience.findIndex(
+        (exp) => exp.title === title
+      );
+
+      if (foundExperience >= 0) {
+        profile.experience[foundExperience] = newExp;
+      } else {
+        profile.experience.unshift(newExp);
+      }
+
+      await profile.save();
+
+      res.json(profile);
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
 module.exports = router;
